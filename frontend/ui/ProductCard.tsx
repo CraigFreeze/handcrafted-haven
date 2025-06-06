2c3daefd8b57e0a43ef7ab4b2a14c7ad3069f269
CraigFreeze <107888709+CraigFreeze@users.noreply.github.com>
2025-06-06 13:08:50 -0700

    remove next footer and add product page routing

--- frontend/ui/ProductCard.tsx
+++ frontend/ui/ProductCard.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { FaRegStar } from "react-icons/fa";
 
 type ProductCardProps = {
+  id: string;
   title: string;
   description: string;
   price: string | number;
@@ -10,31 +11,46 @@ type ProductCardProps = {
   photoSrc: string;
 };
 
-function ProductCard({ title, description, price, rating, photoSrc }: ProductCardProps) {
+function ProductCard({
+  id,
+  title,
+  description,
+  price,
+  rating,
+  photoSrc,
+}: ProductCardProps) {
   return (
     <div className="product-card">
-      <div className="product-card__product-image-wrapper">
-        <Image
-          width={1000}
-          height={667}
-          className="product-card__product-image-wrapper__product-image "
-          src={photoSrc || "/hero-image.jpg"}
-          alt="A duffle bag, dopp kit and ceramic mug sit atop a wood table in front of a wall of art prints."
-        />
-      </div>
-      <div className="product-card__product-details">
-        <div className="product-card__product-details__top">
-          <p className="product-card__product-details__top__product-title">{title}</p>
-          <p className="product-card__product-details__top__rating">{rating} {rating && <FaRegStar />} </p>
+      <a href={"product/" + id}>
+        <div className="product-card__product-image-wrapper">
+          <Image
+            width={1000}
+            height={667}
+            className="product-card__product-image-wrapper__product-image "
+            src={photoSrc || "/hero-image.jpg"}
+            alt="A duffle bag, dopp kit and ceramic mug sit atop a wood table in front of a wall of art prints."
+          />
         </div>
-        <p className="product-card__product-details__product-description">
-          {description}
-        </p>
-        <span className="product-card__product-details__product-price">{price}</span>
-        <button className="product-card__product-details__add-to-cart-button">
-          Add to Cart
-        </button>
-      </div>
+        <div className="product-card__product-details">
+          <div className="product-card__product-details__top">
+            <p className="product-card__product-details__top__product-title">
+              {title}
+            </p>
+            <p className="product-card__product-details__top__rating">
+              {rating} {rating && <FaRegStar />}{" "}
+            </p>
+          </div>
+          <p className="product-card__product-details__product-description">
+            {description}
+          </p>
+          <span className="product-card__product-details__product-price">
+            {price}
+          </span>
+          <button className="product-card__product-details__add-to-cart-button">
+            Add to Cart
+          </button>
+        </div>
+      </a>
     </div>
   );
 }
